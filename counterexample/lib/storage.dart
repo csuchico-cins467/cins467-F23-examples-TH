336491e7655f65e3d483ad2690ab898d6491654c
Bryan Dixon <javawolfpack@gmail.com>
2023-09-14 10:40:10 -0700

    REST/RecipeApp

--- counterexample/lib/storage.dart
+++ counterexample/lib/storage.dart
@@ -1,85 +1,10 @@
 import "package:flutter/foundation.dart";
-import 'package:sqflite/sqflite.dart';
-
-const String tableCount = 'CountTable';
-const String columnId = '_id';
-const String columnCount = 'count';
-
-class CountObject {
-  late int id;
-  late int count;
-
-  Map<String, dynamic> toMap() {
-    var map = <String, dynamic>{
-      columnCount: count,
-    };
-    if (id != null) {
-      map[columnId] = id;
-    }
-    return map;
-  }
-
-  CountObject();
-
-  CountObject.fromMap(Map<dynamic, dynamic> map) {
-    id = map[columnId];
-    count = map[columnCount];
-  }
-}
 
 class CounterStorage {
-  late Database db;
-
   CounterStorage();
 
-  Future open(String path) async {
-    db = await openDatabase(path, version: 1,
-        onCreate: (Database db, int version) async {
-      await db.execute('''
-create table $tableCount (
-  $columnId integer primary key autoincrement,
-  $columnCount integer not null)
-''');
-    });
-  }
-
-  Future<CountObject> insert(CountObject co) async {
-    co.id = await db.insert(tableCount, co.toMap());
-    return co;
-  }
-
-  Future<CountObject> getCount(int id) async {
-    List<Map> maps = await db.query(tableCount,
-        columns: [columnId, columnCount],
-        where: '$columnId = ?',
-        whereArgs: [id]);
-    if (maps.length > 0) {
-      return CountObject.fromMap(maps.first);
-    }
-    CountObject co = CountObject();
-    co.count = 0;
-    co.id = id;
-    co = await insert(co);
-    return co;
-  }
-
-  Future<int> update(CountObject co) async {
-    return await db.update(tableCount, co.toMap(),
-        where: '$columnId = ?', whereArgs: [co.id]);
-  }
-
-  Future close() async => db.close();
-
   Future<bool> writeCounter(int counter) async {
-    try {
-      await open("counter.db");
-      CountObject co = CountObject();
-      co.count = counter;
-      co.id = 0;
-      await update(co);
-      await close();
-      return true;
-    } catch (e) {
+    try {} catch (e) {
       if (kDebugMode) {
         print(e.toString());
       }
@@ -88,12 +13,7 @@ create table $tableCount (
   }
 
   Future<int> readCounter() async {
-    try {
-      await open("counter.db");
-      CountObject co = await getCount(0);
-      await close();
-      return co.count;
-    } catch (e) {
+    try {} catch (e) {
       if (kDebugMode) {
         print(e.toString());
       }
