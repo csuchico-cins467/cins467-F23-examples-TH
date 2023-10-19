0aeaebaa700f4a4ceffcfc73124036c8d355930a
Bryan Dixon <javawolfpack@gmail.com>
2023-10-19 10:07:27 -0700

    Simple Testing Multi-file

--- counter_test/test/widget_test.dart
+++ counter_test/test/widget_test.dart
@@ -11,44 +11,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:counter_test/main.dart';
 
 void main() {
-  group("Counter Class", () {
-    test('Counter value should start at 0', () {
-      expect(Counter().count, 0);
-    });
-
-    test('Counter value should be incremented', () {
-      final counter = Counter();
-      counter.increment();
-      expect(counter.count, 1);
-    });
-
-    test('Counter value should be decremented', () {
-      final counter = Counter();
-      counter.decrement();
-      expect(counter.count, -1);
-    });
-
-    test('Counter value should be added by more than one', () {
-      final counter = Counter();
-      counter.add(2);
-      expect(counter.count, 2);
-    });
-
-    test('Counter value should be subtracted by more than one', () {
-      final counter = Counter();
-      counter.add(-2);
-      expect(counter.count, -2);
-    });
-
-    test('Counter value should be reset', () {
-      final counter = Counter();
-      counter.add(-2);
-      expect(counter.count, -2);
-      counter.reset();
-      expect(counter.count, 0);
-    });
-  });
-
   testWidgets('Counter decrements smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
